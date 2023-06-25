56c0b547b852ace0f79c3cc4c33679dfe61003de
nurieecodes <ndc.e961121@gmail.com>
2023-06-25 16:55:59 -0500

    Create project

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,48 @@
-import { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
-import { ethers } from 'ethers'
-
-// Components
-import Navigation from './Navigation';
-import Loading from './Loading';
-
-// ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
-
-// Config: Import your network config here
-// import config from '../config.json';
-
+import logo from '../logo.png';
+import './App.css';
+ 
 function App() {
-  const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(0)
-
-  const [isLoading, setIsLoading] = useState(true)
-
-  const loadBlockchainData = async () => {
-    // Initiate provider
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-
-    // Fetch accounts
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
-
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
-
-    setIsLoading(false)
-  }
-
-  useEffect(() => {
-    if (isLoading) {
-      loadBlockchainData()
-    }
-  }, [isLoading]);
-
-  return(
-    <Container>
-      <Navigation account={account} />
-
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
-
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
-          <p className='text-center'>Edit App.js to add your code here.</p>
-        </>
-      )}
-    </Container>
-  )
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 ms-3"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dapp University
+        </a>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mx-auto mt-5">
+              <a
+                href="http://www.dappuniversity.com/bootcamp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={logo} className="App-logo" alt="logo"/>
+              </a>
+              <h1 className= "mt-5">Dapp University Starter Kit</h1>
+              <p>
+                Edit <code>src/frontend/components/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="http://www.dappuniversity.com/bootcamp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+              </a>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
